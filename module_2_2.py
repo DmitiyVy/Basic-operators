0775eaf6bd4116e17dfea9be90e29095daddad4c
DmitiyVy <dmitriy.jons@gmail.com>
2024-11-21 21:49:28 +0700

    Update module_2_2.py
    update после комментария преподавателя

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,9 @@
 first = int(input("Введите первое число: "))
 second = int(input("Введите второе число: "))
 thrid = int(input("Введите третье число: "))
-if first == second and first == thrid:
+if len(set([first, second, thrid])) == 1: #if first == second == thrid:
     print("Одинаковых чисел: 3")
-elif first == second or first == thrid:
+elif len(set([first, second, thrid])) == 2: #elif first == second or first == thrid or second == thrid:
     print("Одинаковых чисел: 2")
 else:
-    print("Одинаковых чисел: 0")
+    print("Одинаковых чисел: 0") # в комментариях указан старый код
