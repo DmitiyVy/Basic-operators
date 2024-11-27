4d778c70cb17aeafffbae3f791be7fa01e160651
DmitiyVy <dmitriy.jons@gmail.com>
2024-11-27 22:01:19 +0700

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,23 +1,18 @@
 import random
 x = random.randint(3, 20)
-
 def password_2():
     correct_number = []
     a = 1
     b = 2
     while a < b:
-        for i in range(1, x):
-            a = i
-            for j in range(2, x):
-                b = j
-                if i >= j:
+        for a in range(1, x):
+            for b in range(2, x):
+                if a >= b:
                     continue
-                if i + j == x or x % (i + j) == 0:
-                    correct_number.append(i)
-                    correct_number.append(j)
-
+                if a + b == x or x % (a + b) == 0:
+                    correct_number.append(a)
+                    correct_number.append(b)
         continue
     return correct_number
-
 print("Число из первой вставки - ", x)
 print("Пароль: ", *password_2())
